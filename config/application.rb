11cedfdb3455d81027ed30d2da002293545019ae
neutligen <neutligen@gmail.com>
2015-08-24 16:30:28 +0900

    add config

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,9 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+# For Heroku
+config.assets.initialize_on_precompile = false
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
